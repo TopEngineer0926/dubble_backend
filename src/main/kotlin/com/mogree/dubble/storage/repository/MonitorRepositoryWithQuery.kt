c50cfc40e76bee9161e7166b2552b8c99dbabf5e
TopEngineer0926 <top_engineer_0926@outlook.com>
2021-09-21 23:40:19 +0800

    fix: sorted the monitor table with sendingDate field

--- src/main/kotlin/com/mogree/dubble/storage/repository/MonitorRepositoryWithQuery.kt
+++ src/main/kotlin/com/mogree/dubble/storage/repository/MonitorRepositoryWithQuery.kt
@@ -11,12 +11,14 @@ interface MonitorRepositoryWithQuery : CrudRepository<MonitorEntity, Int> {
 
     companion object {
         const val TABLE = Config.Database.TABLE_MONITOR
+        const val DATE_COLUMN = "sendingDate"
     }
 
     @Query(
             "SELECT * " +
                     "FROM " + TABLE +
                     " WHERE userId = :currentUserId" +
+                    " ORDER BY " + DATE_COLUMN + " DESC" +
                     " LIMIT :offset , :limit", nativeQuery = true
     )
     fun findAll(offset: Int?, limit: Int?, currentUserId: Long?): List<MonitorEntity>
